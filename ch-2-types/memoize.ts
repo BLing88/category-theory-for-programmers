2394c7a7452b10b3867aa3c581030b8b742efad7
Brandon Ling <brandonvling@gmail.com>
2020-07-07 12:58:17 -0400

    Add Fibonacci test case

--- ch-2-types/memoize.ts
+++ ch-2-types/memoize.ts
@@ -10,3 +10,22 @@ function memoize<X, Y>(f: (x: X) => Y): (x: X) => Y {
     }
   };
 }
+
+const naiveFibonacci = (n: number): number => {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return naiveFibonacci(n - 1) + naiveFibonacci(n - 2);
+  }
+};
+
+const start = process.hrtime.bigint();
+console.log(naiveFibonacci(20));
+console.log("non-memoized:", process.hrtime.bigint() - start);
+
+const memoizedFib = memoize(naiveFibonacci);
+const start2 = process.hrtime.bigint();
+console.log(memoizedFib(20));
+console.log("memoized:", process.hrtime.bigint() - start2);
