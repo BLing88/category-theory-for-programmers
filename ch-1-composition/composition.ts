b5c490eaa555bf246bc59b823c7791830dcf68c8
Brandon Ling <brandonvling@gmail.com>
2020-07-31 14:34:28 -0400

    Comment out example functions

--- ch-1-composition/composition.ts
+++ ch-1-composition/composition.ts
@@ -2,25 +2,25 @@ export function compose<X, Y, Z>(f: (x: X) => Y, g: (y: Y) => Z): (x: X) => Z {
   return (x) => g(f(x));
 }
 
-const signString = (x: number) => {
-  if (x > 0) {
-    return "positive";
-  } else if (x === 0) {
-    return "zero";
-  } else {
-    return "negative";
-  }
-};
+// const signString = (x: number) => {
+//   if (x > 0) {
+//     return "positive";
+//   } else if (x === 0) {
+//     return "zero";
+//   } else {
+//     return "negative";
+//   }
+// };
 
-const sign = (y: string) => {
-  if (y === "positive") {
-    return 1;
-  } else if (y === "zero") {
-    return 0;
-  } else if (y === "negative") {
-    return -1;
-  }
-};
+// const sign = (y: "positive" | "zero" | "negative") => {
+//   if (y === "positive") {
+//     return 1;
+//   } else if (y === "zero") {
+//     return 0;
+//   } else if (y === "negative") {
+//     return -1;
+//   }
+// };
 
 // console.log(compose(signString, sign)(-10));
 // console.log(compose(signString, sign)(1223));
